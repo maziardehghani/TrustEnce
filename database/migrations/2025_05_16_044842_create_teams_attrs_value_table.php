cd062758e10eacfc6fbdcc12787584b7e747d939
maziardehghani <maziardehghani1380@gmail.com>
2025-05-16 08:26:34 +0330

    create teams attr , teams attr title , teams attr value migrations

---
+++ database/migrations/2025_05_16_044842_create_teams_attrs_value_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teams_attrs_value', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('att_title_id')
+                ->constrained('teams_attrs_title');
+            $table->string('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teams_attrs_value');
+    }
+};
