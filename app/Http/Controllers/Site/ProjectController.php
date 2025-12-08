3ddab821e85f10c8b7e5ca07bf3d0243dc8892db
maziardehghani <maziardehghani1380@gmail.com>
2025-12-08 05:43:59 +0330

    add

--- app/Http/Controllers/Site/ProjectController.php
+++ app/Http/Controllers/Site/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function lastProjects($limit)
     {
-        $projects = Project::query()->whereActive()->latest()->limit($limit)->get();
+        $projects = Project::query()->whereActive()->get()->limit($limit)->get();
 
         return response()->success(ProjectResources::collection($projects));
     }
